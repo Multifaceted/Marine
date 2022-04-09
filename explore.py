1322bae961f3a337a1afbcc0d2a3a8fd17abb885
Multifaceted <qitianma@outlook.com>
2022-04-09 15:20:20 +0200

    fix import re and maintain unit

--- explore.py
+++ explore.py
@@ -3,6 +3,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
+import re
 import statsmodels.api as sm
 
 data_path = "data"
@@ -14,7 +15,7 @@ file4 = "TBaia_01m-Winkler.txt"
 
 def convertTime(x):
     try:
-        return pd.to_datetime(x["Data"] + "/" + x["Ora"], format="%d/%m/%Y/%H:%M:%S")
+        return pd.to_datetime(x["Data"] + "/" + x["Ora(UTC)"], format="%d/%m/%Y/%H:%M:%S")
     except:
         return -1
 
@@ -22,14 +23,16 @@ def convertTime(x):
 Conducibilita_raw_df = pd.read_csv(os.path.join(data_path, file1), encoding='cp1252', header=None, skiprows=11)
 Conducibilita_raw_df.iloc[0, 0] = re.sub("#", "", Conducibilita_raw_df.iloc[0, 0]).strip()
 Conducibilita_raw_df = Conducibilita_raw_df.squeeze().str.strip().apply(lambda x: re.sub("\s+", ",", x)).str.split(",", expand=True)
-colNames = Conducibilita_raw_df.iloc[0, :].apply(lambda x: re.split(r"[(\\)\'_]", x)[0])
+# colNames = Conducibilita_raw_df.iloc[0, :].apply(lambda x: re.split(r"[(\\)\'_]", x)[0])
+colNames = Conducibilita_raw_df.iloc[0, :]
+
 Conducibilita_raw_df.columns = colNames
 Conducibilita_raw_df = Conducibilita_raw_df.iloc[1:, :]
 
 for j in range(2, Conducibilita_raw_df.shape[1]):
     Conducibilita_raw_df.iloc[:, j] = Conducibilita_raw_df.iloc[:, j].astype(np.float32)
 
-Conducibilita_raw_df["Time"] = Conducibilita_raw_df[["Data", "Ora"]].apply(lambda x: convertTime(x), axis=1)
+Conducibilita_raw_df["Time"] = Conducibilita_raw_df[["Data", "Ora(UTC)"]].apply(lambda x: convertTime(x), axis=1)
 
 Conducibilita_raw_df.info()
 Conducibilita_raw_df.describe()
@@ -39,35 +42,39 @@ Conducibilita_raw_df.describe()
 CTD_raw_df = pd.read_csv(os.path.join(data_path, file2), encoding='cp1252', header=None, skiprows=15)
 CTD_raw_df.iloc[0, 0] = re.sub("#", "", CTD_raw_df.iloc[0, 0]).strip()
 CTD_raw_df = CTD_raw_df.squeeze().str.strip().apply(lambda x: re.sub("\s+", ",", x)).str.split(",", expand=True)
-colNames = CTD_raw_df.iloc[0, :].apply(lambda x: re.split(r"[(\\)\'_]", x)[0])
+# colNames = CTD_raw_df.iloc[0, :].apply(lambda x: re.split(r"[(\\)\'_]", x)[0])
+colNames = CTD_raw_df.iloc[0, :]
+
 CTD_raw_df.columns = colNames
 CTD_raw_df = CTD_raw_df.iloc[1:, :]
 
 for j in range(2, CTD_raw_df.shape[1]):
     CTD_raw_df.iloc[:, j] = CTD_raw_df.iloc[:, j].astype(np.float32)
 
-CTD_raw_df["Time"] = CTD_raw_df[["Data", "Ora"]].apply(lambda x: convertTime(x), axis=1)
+CTD_raw_df["Time"] = CTD_raw_df[["Data", "Ora(UTC)"]].apply(lambda x: convertTime(x), axis=1)
 
 CTD_raw_df.info()
 CTD_raw_df.describe()
 
-sns.lineplot(data=CTD_raw_df, x="Profondita", y="Pressione", marker="o")
+sns.lineplot(data=CTD_raw_df, x="Profondita'(m)", y="Pressione(db)", marker="o")
 
-sm.OLS(CTD_raw_df["Conducibilita"], CTD_raw_df[["Pressione", "Salinita", "Temperatura"]]).fit().summary()
-sm.OLS(CTD_raw_df["Conducibilita"], CTD_raw_df[["Salinita", "Temperatura"]]).fit().summary()
+sm.OLS(CTD_raw_df["Conducibilita'(mS/cm)"], CTD_raw_df[["Pressione(db)", "Salinita'(PSU)", "Temperatura(°C)"]]).fit().summary()
+sm.OLS(CTD_raw_df["Conducibilita'(mS/cm)"], CTD_raw_df[["Salinita'(PSU)", "Temperatura(°C)"]]).fit().summary()
 
 ################### Ossigeno ###################
 Ossigeno_raw_df = pd.read_csv(os.path.join(data_path, file3), encoding='cp1252', header=None, skiprows=11)
 Ossigeno_raw_df.iloc[0, 0] = re.sub("#", "", Ossigeno_raw_df.iloc[0, 0]).strip()
 Ossigeno_raw_df = Ossigeno_raw_df.squeeze().str.strip().apply(lambda x: re.sub("\s+", ",", x)).str.split(",", expand=True)
-colNames = Ossigeno_raw_df.iloc[0, :].apply(lambda x: re.split(r"[(\\)\'_]", x)[0])
+# colNames = Ossigeno_raw_df.iloc[0, :].apply(lambda x: re.split(r"[(\\)\'_]", x)[0])
+colNames = Ossigeno_raw_df.iloc[0, :]
+
 Ossigeno_raw_df.columns = colNames
 Ossigeno_raw_df = Ossigeno_raw_df.iloc[1:, :]
 
 for j in range(2, Ossigeno_raw_df.shape[1]):
     Ossigeno_raw_df.iloc[:, j] = Ossigeno_raw_df.iloc[:, j].astype(np.float32)
 
-Ossigeno_raw_df["Time"] = Ossigeno_raw_df[["Data", "Ora"]].apply(lambda x: convertTime(x), axis=1)
+Ossigeno_raw_df["Time"] = Ossigeno_raw_df[["Data", "Ora(UTC)"]].apply(lambda x: convertTime(x), axis=1)
 
 Ossigeno_raw_df.info()
 Ossigeno_raw_df.describe()
@@ -76,14 +83,16 @@ Ossigeno_raw_df.describe()
 Winkler_raw_df = pd.read_csv(os.path.join(data_path, file4), encoding='cp1252', header=None, skiprows=10)
 Winkler_raw_df.iloc[0, 0] = re.sub("#", "", Winkler_raw_df .iloc[0, 0]).strip()
 Winkler_raw_df = Winkler_raw_df.squeeze().str.strip().apply(lambda x: re.sub("\s+", ",", x)).str.split(",", expand=True)
-colNames = Winkler_raw_df.iloc[0, :].apply(lambda x: re.split(r"[(\\)\'_]", x)[0])
+colNames = Winkler_raw_df.iloc[0, :]
+# colNames = Winkler_raw_df.iloc[0, :].apply(lambda x: re.split(r"[(\\)\'_]", x)[0])
+
 Winkler_raw_df.columns = colNames
 Winkler_raw_df = Winkler_raw_df.iloc[1:, :]
 
 for j in range(2,  Winkler_raw_df.shape[1]):
     Winkler_raw_df.iloc[:, j] = Winkler_raw_df.iloc[:, j].astype(np.float32)
 
-Winkler_raw_df["Time"] = Winkler_raw_df[["Data", "Ora"]].apply(lambda x: convertTime(x), axis=1)
+Winkler_raw_df["Time"] = Winkler_raw_df[["Data", "Ora(UTC)"]].apply(lambda x: convertTime(x), axis=1)
 
 Winkler_raw_df.info()
 Winkler_raw_df.describe()

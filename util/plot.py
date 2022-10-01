e860ba16f7a33fcd6b9bf9307f78d720c54edd96
Multifaceted <qitianma@outlook.com>
2022-10-01 17:50:36 +0200

    fix bugs

--- util/plot.py
+++ util/plot.py
@@ -9,9 +9,9 @@ def plot_average(model, df, n_predictions):
     avgm = np.zeros_like(x_tst[..., 0])
     m = np.mean([np.squeeze(yhat.mean()) for yhat in yhats], axis=0)
     s = np.mean([np.squeeze(yhat.stddev()) for yhat in yhats], axis=0)
-    plt.plot(df["Time_rounded"], m, 'r', label='ensemble means' if i == 0 else None, linewidth=1.,color="red")
-    plt.plot(df["Time_rounded"], m + 2 * s, 'g', linewidth=0.5, label='ensemble means + 2 ensemble stdev' if i == 0 else None, color="green");
-    plt.plot(df["Time_rounded"], m - 2 * s, 'g', linewidth=0.5, label='ensemble means - 2 ensemble stdev' if i == 0 else None, color="green");
+    plt.plot(df["Time_rounded"], m, 'r', label='ensemble means')
+    plt.plot(df["Time_rounded"], m + 2 * s, 'g', linewidth=0.5, label='ensemble means + 2 ensemble stdev', color="green");
+    plt.plot(df["Time_rounded"], m - 2 * s, 'g', linewidth=0.5, label='ensemble means - 2 ensemble stdev', color="green");
     plt.plot(df["Time_rounded"], df["Ossigeno(mg/l)_Ossigeno"], label="True", color="blue")
     plt.legend()
     plt.title("aleatoric uncertainty")

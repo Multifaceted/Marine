c11df37e93ef0192b13522ad9c84beb13752a6ab
Multifaceted <qitianma@outlook.com>
2022-10-04 09:27:37 +0200

    add data_pipeline

--- util/interpolate.py
+++
@@ -1,9 +0,0 @@
-def interpolate(df, name_ls, method, **kwargs):
-    assert method in ["linear", "slinear", "cubic", "spline", "cubicspline", "polynomial"]
-
-    if method == "polynomial":
-        assert type(kwargs["order"]) == type(0)
-    for name in name_ls:
-        df[name] = df[name].interpolate(method=method, **kwargs)
-
-    return df

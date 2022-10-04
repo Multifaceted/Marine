203ee3dde085f0adaae97e8ff468fbdfd57362fb
Multifaceted <qitianma@outlook.com>
2022-10-04 15:24:32 +0200

    add plot for post comparison

--- util/plot.py
+++ util/plot.py
@@ -1,5 +1,43 @@
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
+from functools import partial
+from util.model import init_model_stochastic, init_model_aleatoric
+from util.load_data import data_piepline
+from util.prior_posterior import  posterior_mean_field_with_initializer, prior_trainable_with_initializer
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+
+method = "polynomial"
+order = 5
+
+CTD_Ossigeno_Conducibilita_df = data_piepline(method=method, data_path="../data", resample=False, order=order)
+
+negloglik = lambda y, rv_y: -rv_y.log_prob(y)
+
+posterior_mean_field = partial(posterior_mean_field_with_initializer, initializer="zero")
+prior_trainable = partial(prior_trainable_with_initializer, initializer="zero")
+
+model_MF = init_model_stochastic(n_inputs=7, posterior=posterior_mean_field, prior=prior_trainable, kl_weight=1./2)
+
+model_MF.load_weights("/home/3068020/Marine/checkpoints/stochastic_initialized_seed24_delta1")
+
+m, s = plot_average(model_MF, CTD_Ossigeno_Conducibilita_df, 10)
+np.mean((CTD_Ossigeno_Conducibilita_df["Ossigeno(mg/l)_Ossigeno"] - m) ** 2)
+
+model = init_model_stochastic(n_inputs=7, posterior=posterior_mean_field, prior=prior_trainable, kl_weight=1./2)
+
+model.load_weights("/home/3068020/Marine/checkpoints/stochastic_seed24")
+
+m, s = plot_average(model_MF, CTD_Ossigeno_Conducibilita_df, 10)
+np.mean((CTD_Ossigeno_Conducibilita_df["Ossigeno(mg/l)_Ossigeno"] - m) ** 2)
+
 
 def plot_average(model, df, n_predictions):
     plt.clf()
@@ -16,3 +54,5 @@ def plot_average(model, df, n_predictions):
     plt.legend()
     plt.title("aleatoric uncertainty")
     plt.show()
+
+    return m, s

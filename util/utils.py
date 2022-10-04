455165106b64806c96c92b03324e52dd1657328c
Multifaceted <qitianma@outlook.com>
2022-10-04 09:32:52 +0200

    move interpolate function here

--- util/utils.py
+++ util/utils.py
@@ -16,3 +16,13 @@ def round_time(x):
         x = x.replace(minute=minute)
     x = x.replace(second=0)
     return x
+
+def interpolate(df, name_ls, method, **kwargs):
+    assert method in ["linear", "slinear", "cubic", "spline", "cubicspline", "polynomial"]
+
+    if method == "polynomial":
+        assert type(kwargs["order"]) == type(0)
+    for name in name_ls:
+        df[name] = df[name].interpolate(method=method, **kwargs)
+
+    return df

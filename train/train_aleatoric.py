988452a7789e4e9b7f446fc455bb8a2c4f302f5e
Multifaceted <qitianma@outlook.com>
2022-10-09 23:10:49 +0200

    add json parameters

--- train/train_aleatoric.py
+++ train/train_aleatoric.py
@@ -6,25 +6,37 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir) 
 
+from pathlib import Path
 from util.load_data import data_piepline
 import tensorflow as tf
 import tensorflow_probability as tfp
 import pickle
+import argparse
+import json
+
 
 tfd = tfp.distributions
 
-method = "polynomial"
-order = 5
-save_weights_to = "/home/3068020/Marine/checkpoints/aleatoric_seed0"
-save_history_to = "/home/3068020/Marine/history/aleatoric_seed0"
-n_epochs = 6000
-seed = 0
+parser = argparse.ArgumentParser(description='Parameter')
+parser.add_argument("-p", "--param", type=str, required=True)
+
+args = parser.parse_args()
+
+with open(args.param, 'r') as f:
+  params = json.load(f)
+
+method = params["method"]
+order = params["order"]
+save_to = params["save_to"]
+n_epochs = params["n_epochs"]
+seed = params["seed"]
+
 CTD_Ossigeno_Conducibilita_df = data_piepline(method=method, data_path="../data", resample=False, order=order)
 
 shape, n_vars = CTD_Ossigeno_Conducibilita_df.shape
 
 ###################################################################################################################
-
+Path(save_to).mkdir(parents=True, exist_ok=True)
 
 model_aleatoric = tf.keras.Sequential([
     tf.keras.layers.Input(shape=(n_vars-2, )),
@@ -41,6 +53,10 @@ model_aleatoric = tf.keras.Sequential([
 model_aleatoric.compile(optimizer="Adam", loss="mse", metrics=["mae"])
 tf.keras.utils.set_random_seed(seed)
 history = model_aleatoric.fit(CTD_Ossigeno_Conducibilita_df[["Temperatura(°C)_CTD", "Temperatura(°C)_Conducibilita", "Temperatura(°C)_Ossigeno", "Pressione(db)_CTD", "Pressione(db)_Conducibilita", "Pressione(db)_Ossigeno", "Ossigeno(mg/l)_CTD"]], CTD_Ossigeno_Conducibilita_df[["Ossigeno(mg/l)_Ossigeno"]], batch_size=shape, epochs=n_epochs)
-model_aleatoric.save_weights(save_weights_to)
-with open(save_history_to, 'wb') as file_pi:
-       pickle.dump(history.history, file_pi)
+model_aleatoric.save_weights(os.path.join(save_to, "weights"))
+
+with open(os.path.join(save_to, "history"), 'wb') as file_pi:
+    pickle.dump(history.history, file_pi)
+
+with open(os.path.join(save_to, "params"), 'w') as outfile:
+    json.dump(params, outfile, indent=4)
